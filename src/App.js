2776f7008d5bb2c3286485595d8dd6d9d69873e5
Wojciech Berezowski <61703808+wkberezowski@users.noreply.github.com>
2022-05-11 20:46:31 +0200

    added nested routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -11,8 +11,11 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
-          <Route path='/welcome' element={<Welcome />} />
-          <Route path='/products' exact element={<Products />} />
+          <Route path='/' element={<Navigate replace to='/welcome' />} />
+          <Route path='/welcome' element={<Welcome />}>
+            <Route path='new-user' element={<p>Welcome, new user!</p>}></Route>
+          </Route>
+          <Route path='/products' element={<Products />} />
           <Route path='/products/:productId' element={<ProductDetail />} />
         </Routes>
       </main>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Products = () => {
+  // const navigate = useNavigate();
+  // // navigate('/welcome', {replace: true })
+  // // navigate(-1)
+
   return (
     <section>
       <h1>The Products Page</h1>
